366cb5271fa91eec47972d0af1039a9bb3a67c79
Rajkiran <rajkiran@happyfox.com>
2019-07-26 15:44:17 +0530

    Updates

---
+++ precache-manifest.163c118c91ec670b2bceea5f13fa02b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4b3d7005d323a3540013a5f41d95b53",
+    "url": "/contact-manager/index.html"
+  },
+  {
+    "revision": "316050e307598cc57cf0",
+    "url": "/contact-manager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "654bfe1331af9d5f857c",
+    "url": "/contact-manager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "316050e307598cc57cf0",
+    "url": "/contact-manager/static/js/2.b5abe895.chunk.js"
+  },
+  {
+    "revision": "654bfe1331af9d5f857c",
+    "url": "/contact-manager/static/js/main.11c95fa6.chunk.js"
+  },
+  {
+    "revision": "a25dc548b2853493d42e",
+    "url": "/contact-manager/static/js/runtime~main.b61356a3.js"
+  }
+]);
